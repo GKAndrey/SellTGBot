d3f0eb24824a1c5dee23a60b0090ef987a41c5dd
GKAndrey <druzgaandrey@gmail.com>
2022-12-20 17:45:45 +0200

    Создан скелет.

--- Main/Token.py
+++ Main/Token.py
@@ -1,1 +1,11 @@
 token = ""
+admin_list = []
+
+def adm_pls(ID):
+    admin_list.append(ID)
+
+def adm_mns(ID):
+    try:
+        admin_list.remove(ID)
+    except:
+        print("ERROR. NOT FOUND ID.")
